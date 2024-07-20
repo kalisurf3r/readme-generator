7c2a432274cb656e1642d24a934b5006c723186b
kalisurf3r <ayalaarturo925@gmail.com>
2024-07-20 09:20:00 -0700

    trying to add license badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,17 +1,43 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    } else if (license === 'GPL') {
+        return '![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)';
+    } else if (license === 'Apache') {
+        return '![License: Apache](https://img.shields.io/badge/License-Apache-red.svg)';
+    } else if (license === 'BSD') {
+        return '![License: BSD](https://img.shields.io/badge/License-BSD-green.svg)';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-// function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return '[MIT](https://opensource.org/licenses/MIT)';
+    } else if (license === 'GPL') {
+        return '[GPL](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license === 'Apache') {
+        return '[Apache](https://www.apache.org/licenses/LICENSE-2.0)';
+    } else if (license === 'BSD') {
+        return '[BSD](https://opensource.org/licenses/BSD-3-Clause)';
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-
+function renderLicenseSection(license) {
+    if (license === 'None') {
+        return '';
+    } else {
+        return `## License`;
+    }}
 
 function generateMarkdown(data) {
   return `# ${data.title}
@@ -35,7 +61,7 @@ function generateMarkdown(data) {
 
 ### License
 
-**License:** ${data.license}
+**License:** ${renderLicenseBadge(data.license)} ${renderLicenseLink(data.license)}
 
 ---
 
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const generateMarkdown = require('./generateMarkdown.js');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-// TODO: Create an array of questions for user input
 inquirer.prompt([
     {
         type: 'input',
@@ -57,9 +55,3 @@ inquirer.prompt([
             err ? console.error(err) : console.log('Success!'))
     });
 
-// TODO: Create a function to initialize app
-// function init() {}
-
-// Function call to initialize app
-// init();
- 
